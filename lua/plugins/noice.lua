8c1bf7a8721b990f1ee9326a6947a29a83ddfced
Mikael Lindström <mikael.lindstrom@gmail.com>
2024-03-02 22:20:54 +0100

    Add keybinding for scroll in lsp hover

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,52 +5,72 @@ return {
         'MunifTanjim/nui.nvim',
         'rcarriga/nvim-notify',
     },
-    config = function()
-        require('noice').setup({
-            lsp = {
-                override = {
-                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                    ['vim.lsp.util.stylize_markdown'] = true,
-                    ['cmp.entry.get_documentation'] = true,
-                },
+    opts = {
+        lsp = {
+            override = {
+                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                ['vim.lsp.util.stylize_markdown'] = true,
+                ['cmp.entry.get_documentation'] = true,
             },
-            cmdline = {
-                view = 'cmdline',
+        },
+        cmdline = {
+            view = 'cmdline',
+        },
+        routes = {
+            {
+                filter = {
+                    event = 'msg_show',
+                    kind = '',
+                    find = 'written',
+                },
+                opts = { skip = true },
             },
-            routes = {
-                {
-                    filter = {
-                        event = 'msg_show',
-                        kind = '',
-                        find = 'written',
-                    },
-                    opts = { skip = true },
+            {
+                filter = {
+                    event = 'msg_show',
+                    kind = '',
+                    find = 'yanked',
                 },
-                {
-                    filter = {
-                        event = 'msg_show',
-                        kind = '',
-                        find = 'yanked',
-                    },
-                    opts = { skip = true },
-                },
-                {
-                    filter = {
-                        event = 'msg_show',
-                        kind = '',
-                        find = 'fewer line',
-                    },
-                    opts = { skip = true },
-                },
-                {
-                    filter = {
-                        event = 'msg_show',
-                        kind = '',
-                        find = 'more line',
-                    },
-                    opts = { skip = true },
-                },
+                opts = { skip = true },
             },
-        })
-    end,
+            {
+                filter = {
+                    event = 'msg_show',
+                    kind = '',
+                    find = 'fewer line',
+                },
+                opts = { skip = true },
+            },
+            {
+                filter = {
+                    event = 'msg_show',
+                    kind = '',
+                    find = 'more line',
+                },
+                opts = { skip = true },
+            },
+        },
+    },
+    keys = {
+        {
+            '<c-f>',
+            function()
+                if not require('noice.lsp').scroll(4) then return '<c-f>' end
+            end,
+            silent = true,
+            expr = true,
+            desc = 'Scroll forward',
+            mode = { 'i', 'n', 's' },
+        },
+        {
+            '<c-b>',
+            function()
+                if not require('noice.lsp').scroll(-4) then return '<c-b>' end
+            end,
+            silent = true,
+            expr = true,
+            desc = 'Scroll backward',
+            mode = { 'i', 'n', 's' },
+        },
+    },
 }
